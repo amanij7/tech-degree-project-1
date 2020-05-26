12f9a20d19f5bad2ab505cbabcde7e74bb3f59bd
Kaya Jackson <kayajackson27@gmail.com>
2020-05-26 15:17:58 -0400

    third commit

--- js/random.js
+++ js/random.js
@@ -1,31 +1,45 @@
 
-
-
 //select random quote
-function getRandomQuote(arr) {
-    var randomQuote = Math.floor(Math.random() * arr.length);
-        var results = arr[randomQuote];
-        return results;
+function getRandomQuote() {
+    var randomQuote = Math.floor(Math.random() * quotes.length);
+        
+        return quotes[randomQuote];
+}; 
+
+
+// Random color generator
+
+function randomColor() {
+    var color = Math.floor(Math.random() * colors.length );
+
+    var showColor = document.querySelector('body');
+    showColor.style.backgroundColor = colors[color]; 
+}
+
+
+
+// Display quote and random color to UI
+
+function printQuote() {
+   var quoteGenerated = getRandomQuote();
+   var displayQuote = document.getElementById("quote-box");
+
+   var html = `<p class="quote">${quoteGenerated.quote}</p> <p class="source">${quoteGenerated.source}</p>`;
+
+   if (quoteGenerated.citation) {
+       html += `<span class="citation">${quoteGenerated.citation}</span>`;
+   }
+
+   if (quoteGenerated.year) {
+       html += `<span class="year">${quoteGenerated.year}</span>`
+   }
+   
+   
+   displayQuote.innerHTML = html;
+
+   randomColor();
+   
 };
-
-
-
-// Display quote to UI
-function print(message) {
-    document.write(message);
-};
-
-function printQuote(res) {
-   var html = `<p class="quote>${getRandomQuote.quote}</p>"`;
-   html += `<p class="source>${getRandomQuote.source}</p>"`;
-
-   print(html);
-};
-console.log(printQuote(quotes));
-
-
-
-
 
 
 
--- js/script.js
+++ js/script.js
@@ -1,51 +1,67 @@
 
 let quotes = [
     {
-        quote: 'To laugh ar yourself is to love yourself.',
-        source: '- Mickey Mouse'
+        quote: 'To laugh at yourself is to love yourself.',
+        source: ' Mickey Mouse'
     },
     {
         quote: 'You know you\'re in love when you can\'t fall asleep because realisy is finally better than your dreams .',
-        source: '- Dr. Seuss'
+        source: ' Dr. Seuss'
     },
     {
         quote: 'I don\'t have to worry about who doesn\'t like me... I\'m too busy loving the people who love me.',
-        source: '- Snoopy'
+        source: ' Snoopy'
     },
     {
         quote: 'In the book of life, the answers aren\'t in the back.',
-        source: '- Charlie Brown'
+        source: ' Charlie Brown'
     },
     {
         quote: 'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.',
-        source: '- Dr. Seuss'
+        source: ' Dr. Seuss'
     },
     {
         quote: 'Sometimes we have to go deep inside ourselves to solve our problems.',
-        source: '- Patrick Star from Spongebob Squarepants'
+        source: ' Patrick Star from Spongebob Squarepants'
     },
     {
         quote: 'The flower that blooms in adversity is the rarest and beautiful of all.',
-        source: '- The Emperor from Mulan'
+        source: ' The Emperor',
+        citation: 'Mulan',
+        year: '1998'
     },
     {
         quote: 'Just keep swimming.', 
-        source: '- Dory'
+        source: 'Dory',
+        citation: 'Finding Nemo',
+        year: '2003'
     },
     {
         quote: 'Oh yes, the past can hurt. But, you can either run from it or learn from it.',
-        source: '- Rafiki'
+        source: 'Rafiki',
+        citation: 'Lion King',
+        year: '1994'
     },
     {
         quote: 'Life is hard, Tommy. Sometimes I think it\'s the hardest thing there is.',
-        source: '- Chuckie from Rugrats'
+        source: ' Chuckie',
+        citation: 'Rugrats'
     },
     {
         quote: 'I\'m headed home to have myself a juice, snuggle with my blankie, and go nap nap.',
-        source: '- Tommy from Rugrats'
+        source: ' Tommy ',
+        citation: 'Rugrats'
     }
     
 
+];
+
+var colors = [ 
+    '#DAF7A6', 
+    '#FFC300', 
+    '#FF5733', 
+    '#C70039', 
+    '#900C3F' 
 ];
 
 
